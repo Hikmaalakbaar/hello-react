d2ed228b3bea276b1537564a61909ef9f8662645
Hikmaalakbaar <rohiderboy24@gmail.com>
2025-06-27 23:05:11 +0700

    Materi 11: Function Component Dalam 1 File

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { Component } from 'react';
 import Header from './Header';
 
+const Footer = function(){
+  return(
+    <div>
+      <h3>Halaman Footer</h3>
+      <h3>Component ini dibuat menggunakan function bukan class</h3>
+    </div>
+  );
+}
 
 class App extends Component{
   render(){
     return(
       <div>
         <Header/>
-        <h1>Hello World</h1>
+        <h1>Component dari Class App</h1>
+        <Footer/>
       </div>
     );
   }
