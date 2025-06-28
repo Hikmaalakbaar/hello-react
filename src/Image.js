779a638c5d3cc93e155878d19bb4aa6a4c1f9c49
Hikmaalakbaar <rohiderboy24@gmail.com>
2025-06-28 08:40:06 +0700

    Materi 17: Props pada Class Component

--- src/Image.js
+++ src/Image.js
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 class Image extends Component{
     render(){
         return(
-            <img src="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/food1.jpg" alt="Food" width='500'/>
+            <img src={this.props.linkGambar} alt="Food" width='500'/>
         );
     }
 }
--- src/List.js
+++ src/List.js
@@ -7,9 +7,19 @@ class List extends Component{
             <div>
                 <Image/>
                 <ol>
-                    <li>Satu</li>
-                    <li>Dua</li>
-                    <li>Tiga</li>
+                    <li>Satu
+                        <Image linkGambar='https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/food1.jpg'/>
+                    </li>
+                    <li>Dua
+                        <Image linkGambar='https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/nasipadang.jpg'/>
+                    </li>
+                    <li>Tiga
+                        <Image linkGambar='https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sate.png'/>
+                    </li>
+                    <li>
+                        Empat
+                        <Image linkGambar='https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sotolamongan.png'/>
+                    </li>
                 </ol>
             </div>
         );
