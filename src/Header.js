811c43b9869822269883729f299ed43804eec6db
Hikmaalakbaar <rohiderboy24@gmail.com>
2025-06-28 08:46:02 +0700

    Materi 18: Penulisan State

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,19 @@
 import React, {Component} from "react";
 
-export default class Header extends Component{
-  render(){
-    return(
-      <div>
-        <h2>Makanan Khas Indonesia</h2>
-      </div>
-    );
-  }
+class Header extends Component{
+  constructor(props) {
+    super(props);
+    this.state ={
+    judul : 'Ini judul dari state',
+  };
 }
-
+render(){
+  return(
+    <div>
+      <h2>Component dari Class Header</h2>
+      <h3>{this.state.judul}</h3>
+    </div>
+  );
+}
+}
+export default Header;
