c419f2112bba76b39dd83bb598882c2c2506eb9a
Hikmaalakbaar <rohiderboy24@gmail.com>
2025-07-03 01:19:25 +0700

    Materi 37 : List dan Key dari State

--- src/Main.js
+++ src/Main.js
@@ -6,7 +6,22 @@ class Main extends Component{
         this.state = {
             title: 'Menu Makanan',
             title2: 'Menu Minuman',
-            inputValue: 'Hikmal Akbar',
+            inputValue: ' ',
+            inputKota: ' ',
+            namaMakanan: [
+                {
+                    nama: 'Mie Ayam',
+                    harga: 10000
+                },
+                {
+                    nama: 'Bakso',
+                    harga: 12000
+                },
+                {
+                    nama: 'Soto Ayam',
+                    harga: 15000
+                },
+            ],
         }
         this.rubahData = this.rubahData.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -27,7 +42,7 @@ class Main extends Component{
         });
     }
 
-    componentDodMount(){
+    componentDidMount(){
         console.log('Jalan: componentDidMount');
     }
 
@@ -35,10 +50,20 @@ class Main extends Component{
         return(
             <div>
                 <h3>{this.state.title}</h3>
-                <button onClick={this.rubahData}>Ubah Makanan</button>
-                <input type="text" onChange={this.handleChange}/>
-                <input type="text" value={this.state.inputKota} placeholder="kota" onChange={(e) => this.handleChange('inputKota', e)}/>
-            </div>
+                <h2>{this.state.title2}</h2>
+                <button onClick={this.rubahData}>Ubah Makanan</button><br/><br/>
+                <input type="text" value={this.state.inputValue} placeholder="makanan" onChange={(e) => this.handleChange('inputValue', e)}/>
+                <input type="text" value={this.state.inputKota} placeholder="kota" onChange={(e) => this.handleChange('inputKota',e)}/>
+                {this.state.namaMakanan.map((makanan, index) => {
+                    return(
+                        <div key={index+1}>
+                            <p>No {index}</p>
+                            <p>Nama {makanan.nama}</p>
+                            <p>Harga {makanan.harga}</p>
+                        </div>
+                    );
+                })};
+                </div>
         );
     };
 }
